418ef2f8a5c5bba912b5dd4e93133c82fcb19b10
Hao Wang <wang.hao.chnbj@gmail.com>
2013-12-12 16:15:06 -0500

    hooked up space usage notification to UI

--- YoutubeDownloader/Shared/UI/MediaLibrary/YDDeviceSpaceAvailabilityView.m
+++ YoutubeDownloader/Shared/UI/MediaLibrary/YDDeviceSpaceAvailabilityView.m
@@ -9,6 +9,7 @@
 #import "YDDeviceSpaceAvailabilityView.h"
 #import "YDDeviceUtility.h"
 #import "YDConstants.h"
+#import "DownloadTask.h"
 
 @interface YDDeviceSpaceAvailabilityView ()
 {
@@ -28,6 +29,10 @@
     self = [super initWithFrame:frame];
     if (self) {
         _barHeight = frame.size.height;
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(receivesDiskSpaceChangeNotification:)
+                                                     name:kTotalVideoSizeChangeNotification
+                                                   object:nil];
     }
     return self;
 }
@@ -36,6 +41,16 @@
 {
     [super layoutSubviews];
     [self layoutColoredSpaceBarAnimated:NO];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)receivesDiskSpaceChangeNotification:(NSNotification *) notification
+{
+    [self layoutColoredSpaceBarAnimated:YES];
 }
 
 - (void)layoutColoredSpaceBarAnimated:(BOOL)animated
@@ -56,10 +71,13 @@
     uint64_t totalSpace = deviceSpace.totalSpace;
     uint64_t availableSpace = deviceSpace.availableSpace;
     uint64_t usedSpace = totalSpace - availableSpace;
+    uint64_t thisAppSpace = ([[DownloadTask getTotalVideoSizeWithContext:[NSManagedObjectContext MR_contextForCurrentThread]]longLongValue]/1024ll)/1024ll;
+    usedSpace -= thisAppSpace;
     
     CGFloat totalLength = self.frame.size.width;
     CGFloat otherAppSpaceBarWidth = 1.0*usedSpace/totalSpace * totalLength;
     CGFloat availableSpaceBarWidth = 1.0*availableSpace/totalSpace * totalLength;
+    CGFloat thisAppSpaceBarWidth = 1.0*thisAppSpace/totalSpace * totalLength;
     
     if (!self.otherAppSpaceBar) {
         self.otherAppSpaceBar = [[UIView alloc]initWithFrame:CGRectMake(0, 0, otherAppSpaceBarWidth, _barHeight)];
@@ -68,14 +86,22 @@
         self.otherAppSpaceBar.frame = CGRectMake(0, 0, otherAppSpaceBarWidth, _barHeight);
     }
     
+    if (!self.thisAppSpaceBar) {
+        self.thisAppSpaceBar = [[UIView alloc]initWithFrame:CGRectMake(otherAppSpaceBarWidth, 0, thisAppSpaceBarWidth, _barHeight)];
+        self.thisAppSpaceBar.backgroundColor = DEVICE_SPACE_THIS_APP_BAR_COLOR;
+    }else{
+        self.thisAppSpaceBar.frame = CGRectMake(otherAppSpaceBarWidth, 0, thisAppSpaceBarWidth, _barHeight);
+    }
+    
     if (!self.availableSpaceBar) {
-        self.availableSpaceBar = [[UIView alloc]initWithFrame:CGRectMake(otherAppSpaceBarWidth, 0, availableSpaceBarWidth, _barHeight)];
+        self.availableSpaceBar = [[UIView alloc]initWithFrame:CGRectMake(otherAppSpaceBarWidth + thisAppSpaceBarWidth, 0, availableSpaceBarWidth, _barHeight)];
         self.availableSpaceBar.backgroundColor = DEVICE_SPACE_AVAILABLE_BAR_COLOR;
     }else{
         self.availableSpaceBar.frame = CGRectMake(otherAppSpaceBarWidth, 0, availableSpaceBarWidth, _barHeight);
     }
     
     [self addSubview:self.otherAppSpaceBar];
+    [self addSubview:self.thisAppSpaceBar];
     [self addSubview:self.availableSpaceBar];
 }
 
